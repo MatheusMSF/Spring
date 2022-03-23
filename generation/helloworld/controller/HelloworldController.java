3acbe882c5f506f44f0b463ae928fdc65da890d2
Matheus Ferreira <matheus@java.com>
2022-03-23 15:34:05 -0300

    Introducao Spring

--- generation/helloworld/controller/HelloworldController.java
+++ generation/helloworld/controller/HelloworldController.java
@@ -5,12 +5,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/helloworld")
+@RequestMapping
 public class HelloworldController {
 	
-	@GetMapping
+	@GetMapping("/helloworld")
 	public String helloworld() 
 	{
 		return "Hello World!!!";
+	}
+	
+	@GetMapping("/bsm")
+	public String BSM() 
+	{
+		return "Mentalidades da Generation:\nOrientação ao futuro.\nResponsabilidade Pessoal." +
+				"\nMentalidade de crescimento.\nPersistencia." + 
+				"\n\nHabilidades da Generation:\nTrabalho em equipe." +
+				"\nAtençao aos detalhes.\nProatividade.\nComunicação.";
+	}
+	
+	@GetMapping("/objetivos")
+	public String objetivos() 
+	{
+		return "Meus objetivos desta semana: Aprender os termos básicos utilizados no Spring.";
 	}
 }
